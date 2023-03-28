e06fff9b06cfc2b2f3a12374012bf80e5570730a
Barbara Rodrigues <rodriguessbarbara@hotmail.com>
2023-03-28 17:08:52 -0300

    feat: :sparkles: feito parse json e filtrado resultados
    parse json para listar todas as informaç~]oes e filtrado com um Map para pegar o que importa(title, image e imDbRating)

--- src/App.java
+++ src/App.java
@@ -4,23 +4,34 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
   public static void main(String[] args) throws IOException, InterruptedException {
-    System.out.println("Hello World!");
 
     String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-    URI endereco =  URI.create(url);
+    URI endereco = URI.create(url);
     var client = HttpClient.newHttpClient();
     var request = HttpRequest.newBuilder(endereco).GET().build();
     HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
     String body = response.body();
 
-    System.out.println(body);
+    //System.out.println(body);
 
-     //pegar só os dados que interessam nesse caso (titulo, poster, ranking)
+    var parser = new JsonParser();
+    List<Map<String, String>> listaFilmes = parser.parse(body);
 
-     //exibir/manipular os dados 
+    System.out.println(listaFilmes.size());
+
+     //exibir os dados 
+     for (Map<String, String> filme : listaFilmes) {
+      System.out.println(filme.get("title"));
+      System.out.println(filme.get("image"));
+      System.out.println(filme.get("imDbRating"));
+      System.out.println("");
+
+     }
 
   }
 }
