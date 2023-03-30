ee826531055690a029dbb8f5070befb68866b76b
Barbara Rodrigues <rodriguessbarbara@hotmail.com>
2023-03-30 15:32:10 -0300

    feat: :sparkles: criado texto p/ novas figurinhas

--- src/CreateStickers.java
+++ src/CreateStickers.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -7,24 +9,24 @@ import javax.imageio.ImageIO;
 public class CreateStickers {
   
   public void create() throws IOException {
-    //pegar e ler a imagem
     BufferedImage imgOriginal = ImageIO.read(new File("entrada/filme.jpg"));
 
-    //criar nova imagem em memoria (npj e redimencionada)
     int largura = imgOriginal.getWidth();
-    int novaAltura = imgOriginal.getHeight() + 150;
-
+    int novaAltura = imgOriginal.getHeight() + 200;
     BufferedImage novaImg = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
 
     //copiar imagem original p/ nova img (em memória)
     Graphics2D graficos = (Graphics2D) novaImg.getGraphics();
     graficos.drawImage(imgOriginal, 0, 0, null);
 
-    //escrever frase em nova img
+    //escreve frase em nova img
+    var fonte = new Font("Sans_serif", Font.BOLD, 72);
+    graficos.setFont(fonte);
+    graficos.setColor(Color.RED);
+    graficos.drawString("FODA TA", largura/2, novaAltura - 100);
 
-    //escrever nova img no arquivo
+    //escreve nova img no arquivo
     ImageIO.write(novaImg, "png", new File("saida/sticker.png"));
-
   }
 
   public static void main(String[] args) throws IOException {
